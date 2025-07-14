c15b00437f8743c11cce69d70ff0bd8b7f2e1c81
Changmin Lee <107976623+chngmn@users.noreply.github.com>
2025-07-14 16:59:50 +0900

    feat: DB에 족보 업로드 가능하도록 구현

--- models/Quiz.js
+++ models/Quiz.js
@@ -14,7 +14,8 @@ const quizSchema = new mongoose.Schema({
   content:     { type: String, required: true },
   options:     [{ type: String }], // 객관식/ox일 때 사용
   answer:      { type: mongoose.Schema.Types.Mixed, required: true }, // string or array
-  explanation: { type: String }
+  explanation: { type: String },
+  files:       [{ type: String }] // 업로드된 파일 경로를 저장할 필드 추가
 }, { timestamps: true });
 
 module.exports = mongoose.model('Quiz', quizSchema);
